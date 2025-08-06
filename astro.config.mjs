f0590dc3bae721e2d1b9cabf5919de8b55530ecb
Umar <kingshisha.indonesia@gmail.com>
2025-08-06 14:06:55 +0700

    Final code ready for cloud deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+// Konfigurasi ini akan mendeteksi perintah yang dijalankan (dev atau build)
+export default defineConfig(({ command }) => ({
+  // Ganti dengan username GitHub Anda
+  site: 'https://kelar89.github.io',
+  // 'base' hanya akan diterapkan saat 'npm run deploy' (command === 'build')
+  // Saat 'npm run dev', 'base' akan menjadi '/' (root)
+  base: command === 'build' ? '/umroh-website/' : '/',
+  integrations: [tailwind()]
+}));
